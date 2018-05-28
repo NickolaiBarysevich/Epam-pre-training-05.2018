fc93b52e1afa0982165e30ff188bcb5bf7eb6ca7
Nick <36010338+Nickshock@users.noreply.github.com>
2018-05-29 00:50:08 +0300

    Update MoodSensor.java

--- task3/MoodSensor.java
+++ task3/MoodSensor.java
@@ -1,10 +1,10 @@
 /*
- * @(#)MoodSensor.java           1.1 2018
+ * @(#)MoodSensor.java            1.2 2018
  *
  * Copyright 1995-1999 Sun Microsystems, Inc.
  * All rights reserved. Used by permission
  *
- * Last modified: 27.05.18 23:30
+ * Last modified: 29.05.18 0:48
  */
 
 package com.nickshock.task3;
@@ -12,7 +12,7 @@ package com.nickshock.task3;
 /**
  * This class defines user's mood
  *
- * @version          1.1 27 May 2018
+ * @version          1.2 29 May 2018
  * @author           Barysevich Nikalai
  */
 
@@ -24,17 +24,21 @@ public class MoodSensor {
     public static final String OK = ":|";
     public static final String LOL = ":D";
 
+    public static final int BORDER_FOR_SAD = 25;
+    public static final int BORDER_FOR_OK = 50;
+    public static final int BORDER_FOR_HAPPY = 75;
+
     /** Defines mood*/
     public static String getMood() {
         int rand = (int) (Math.random() * 100);
 
         String result;
 
-        if (rand < 25) {
+        if (rand < BORDER_FOR_SAD) {
             result = SAD;
-        } else if (rand < 50) {
+        } else if (rand < BORDER_FOR_OK) {
             result = OK;
-        } else if (rand < 75) {
+        } else if (rand < BORDER_FOR_HAPPY) {
             result = HAPPY;
         } else {
             result = LOL;
@@ -43,3 +47,4 @@ public class MoodSensor {
         return result;
     }
 }
+
