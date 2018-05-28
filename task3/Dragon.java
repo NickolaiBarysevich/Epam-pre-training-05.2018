24de5d2bbaf5a44c985a9a115774b5192b90aab3
Nick <36010338+Nickshock@users.noreply.github.com>
2018-05-29 00:46:05 +0300

    Update Dragon.java

--- task3/Dragon.java
+++ task3/Dragon.java
@@ -1,10 +1,10 @@
 /*
- * @(#)Dragon.java            1.1 2018
+ * @(#)Dragon.java            1.2 2018
  *
  * Copyright 1995-1999 Sun Microsystems, Inc.
  * All rights reserved. Used by permission
  *
- * Last modified: 27.05.18 23:30
+ * Last modified: 29.05.18 00:45
  */
 
 package com.nickshock.task3;
@@ -12,7 +12,7 @@ package com.nickshock.task3;
 /**
  * This class count dragon's heads and eyes
  *
- * @version          1.1 27 May 2018
+ * @version          1.2 29 May 2018
  * @author           Barysevich Nikalai
  */
 
@@ -20,6 +20,8 @@ public class Dragon {
 
     public static final int HEADS_BEFORE201 = 3;
     public static final int HEADS_AFTER200 = 2;
+    public static final int BORDER_FOR_200 = 200;
+    public static final int BORDER_FOR_300 = 300;
 
 
     /** Count heads and eyes*/
@@ -34,15 +36,15 @@ public class Dragon {
 
         int heads = years;
 
-        if (years < 201) {
+        if (years <= BORDER_FOR_200) {
             heads *= HEADS_BEFORE201;
-        } else if (years < 301) {
-            heads = 200 * HEADS_BEFORE201;
-            heads += (years - 200) * HEADS_AFTER200;
+        } else if (years <= BORDER_FOR_300) {
+            heads = BORDER_FOR_200 * HEADS_BEFORE201;
+            heads += (years - BORDER_FOR_200) * HEADS_AFTER200;
         } else {
-            heads = 200 * HEADS_BEFORE201;
+            heads = BORDER_FOR_200 * HEADS_BEFORE201;
             heads += 100 * HEADS_AFTER200;
-            heads += years - 300;
+            heads += years - BORDER_FOR_300;
         }
 
         result = "Dragon has " + heads + " and " + heads * 2 + " eyes.";
