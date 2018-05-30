c857eabdd73741edc722d1549705031bb2e75579
Nick <36010338+Nickshock@users.noreply.github.com>
2018-05-30 11:10:27 +0300

    Update Calendar.java

--- task3/Calendar.java
+++ task3/Calendar.java
@@ -72,8 +72,8 @@ public class Calendar {
     public static boolean isLeapYear(int year) {
         boolean flag = false;
 
-        if (year % 4 == 0 & year % 100 == 0 & year % 400 == 0) flag = true;
-        else if (year % 4 == 0 & year % 100 == 0) ;
+        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) flag = true;
+        else if (year % 4 == 0 && year % 100 == 0) ;
         else if (year % 4 == 0) flag = true;
 
         return flag;
