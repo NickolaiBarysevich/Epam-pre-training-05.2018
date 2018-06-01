85800425bea8c6b529c880e1998ccdac260f77df
Nick <36010338+Nickshock@users.noreply.github.com>
2018-06-01 18:52:35 +0300

    Update DifferentDigits.java

--- task4/DifferentDigits.java
+++ task4/DifferentDigits.java
@@ -1,10 +1,10 @@
 /*
- * @(#)DifferentDigits.java           1.7 2018
+ * @(#)DifferentDigits.java           1.8 2018
  *
  * Copyright 1995-1999 Sun Microsystems, Inc.
  * All rights reserved. Used by permission
  *
- * Last modified: 01.06.18 0:52
+ * Last modified: 01.06.18 18:49
  *
  */
 
@@ -13,13 +13,13 @@ package com.nickshock.task4;
 /**
  * This class calculates the amount of the different digits in a number
  *
- * @version          1.7 1 June 2018
+ * @version          1.8 1 June 2018
  * @author           Barysevich Nikalai
  */
 
 public class DifferentDigits {
 
-    /** Return the amount of a different digits*/
+    /** Return the amount of the different digits*/
     public static int countDifferentDigits(int number) {
 
         int amountOfSameDigits = 0;
@@ -29,7 +29,7 @@ public class DifferentDigits {
         while (number != 0) {
             digit = MaxDigit.getLastDigit(number);
 
-            if (countDigitInNumber(number, digit) > 1) {
+            if (!isSingleDigit(number, digit)) {
                 amountOfSameDigits++;
             }
 
@@ -39,10 +39,11 @@ public class DifferentDigits {
         return length - amountOfSameDigits;
     }
 
-    /** Return the of a one digit in a number*/
-    public static int countDigitInNumber(int number, int countingDigit) {
+    /** Return true is digit is single in a number*/
+    public static boolean isSingleDigit(int number, int countingDigit) {
         int amount = 0;
 
+        boolean result = true;
         int digit;
 
         while (number != 0) {
@@ -51,10 +52,15 @@ public class DifferentDigits {
             if (digit == countingDigit) {
                 amount++;
             }
+
+            if (amount > 1) {
+                result = false;
+                break;
+            }
             number /= 10;
         }
 
-        return amount;
+        return result;
     }
 
     /** Returns length of a number*/
