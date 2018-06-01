f39ffa1d3db6517540a45efd528179bf3900b861
Николай Барисевич <nick.barysevich@gmail.com>
2018-06-01 20:27:19 +0300

    21

---
+++ task4/Main.java
@@ -0,0 +1,10 @@
+package com.nickshock;
+
+import com.nickshock.task4.DifferentDigits;
+import com.nickshock.task4.NodAndNok;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DifferentDigits.countDifferentDigits(12344));
+    }
+}
