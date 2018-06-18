cae6348095baaa5a405ec3c2f5648100323bd4b2
Николай Барисевич <nick.barysevich@gmail.com>
2018-06-19 01:37:24 +0300

    flowcharts fixed

--- task6/MatrixWorker.java
+++ task6/MatrixWorker.java
@@ -158,16 +158,21 @@ public class MatrixWorker {
     public static int[] findLocalMin(int[][] matrix) {
         validateArguments(matrix);
 
+        int targetI = -1;
+        int targetJ = -1;
+
         for (int i = 1; i < matrix.length - 1; i++) {
             for (int j = 1; j < matrix[i].length - 1; j++) {
                 if (matrix[i][j] < matrix[i - 1][j] && matrix[i][j] < matrix[i][j + 1] && matrix[i][j] < matrix[i + 1][j]
                         && matrix[i][j] < matrix[i][j + 1]) {
-                    return new int[]{i, j};
+                    targetI = i;
+                    targetJ = j;
+                    break;
                 }
             }
         }
 
-        return new int[]{-1, -1};
+        return new int[]{targetI, targetJ};
     }
 
     /**
@@ -182,17 +187,22 @@ public class MatrixWorker {
     public static int[] findLocalMax(int[][] matrix) {
         validateArguments(matrix);
 
+        int targetI = -1;
+        int targetJ = -1;
+
         for (int i = 1; i < matrix.length - 1; i++) {
             for (int j = 1; j < matrix[i].length - 1; j++) {
                 if (matrix[i][j] > matrix[i - 1][j] && matrix[i][j] > matrix[i][j + 1]
                         && matrix[i][j] > matrix[i + 1][j]
                         && matrix[i][j] > matrix[i][j + 1]) {
-                    return new int[]{i, j};
+                    targetI = i;
+                    targetJ = j;
+                    break;
                 }
             }
         }
 
-        return new int[]{-1, -1};
+        return new int[]{targetI, targetJ};
     }
 
 
